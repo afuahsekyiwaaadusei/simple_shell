94e5e78ef041da4df888fa152e83f5a5b4bcbaab
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2023-08-25 12:35:39 -0700

    env builtin

--- shell.c
+++ shell.c
@@ -65,12 +65,30 @@ int main(void)
  */
 void builtin(char **line, char ***argv, char **dup_str)
 {
+	int status = 0, len, i = 0;
+	char buf[] = "\n";
+
 	if ((_strcmp((*argv)[0], "exit")) == 0)
 	{
 		free(*line);
 		free(*argv);
 		free(*dup_str);
-		exit(errno);
+		exit(status);
+	}
+	if ((_strcmp((*argv)[0], "env")) == 0)
+	{
+		while (environ[i])
+		{
+			len = _strlen(environ[i]);
+			write(STDOUT_FILENO, environ[i], len);
+			write(STDOUT_FILENO, buf, sizeof(buf));
+			i++;
+		}
+		free(*line);
+		free(*argv);
+		free(*dup_str);
+		exit(status);
+
 	}
 	return;
 
